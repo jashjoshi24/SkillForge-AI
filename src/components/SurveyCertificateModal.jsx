4d3ee1c1b8f3b82ab57e5796e0ec2bb52a83efc1
jashjoshi24 <jashjoshi84@gmail.com>
2026-08-14 10:51:17 +0530

    changes

--- src/components/SurveyCertificateModal.jsx
+++ src/components/SurveyCertificateModal.jsx
@@ -1,4 +1,4 @@
-git switch mainimport React from 'react';
+import React from 'react';
 import { useApp } from '../context/AppContext';
 import { Compass, ShieldCheck, Printer, X, Sparkles, Award, MapPin } from 'lucide-react';
 
